176e20369dca3ce99ea01948b98d2ae8775aabdd
ahbarnett <alex.h.barnett@gmail.com>
2022-04-05 19:09:28 -0400

    debug Matern-1/2 in dim>1; FT checks out in test

--- test/test_kers.m
+++ test/test_kers.m
@@ -1,6 +1,6 @@
 % script to test all kernel functions have roughly the right Fourier transform,
 % using equispaced quadrature (PTR) Kronecker product over dims.
-% For now, only SE kernel.
+% For now, only SE kernel and Matern-1/2.
 % Barnett 4/5/22
 clear
 
@@ -10,11 +10,13 @@ xid = 3.0;   % freq mag |xi| to test (let's make all kernels use same for now)
 for type=1:2   % -------------- kernel types --------------
   for dim=1:3      % .......
     switch type        
-      case 1        % SE
+      case 1
+        nam = 'SE';
         ker = SE_ker(dim,l);
         h = l/2;         % small enough spacing for emach on Gaussian
         L = l*sqrt(2*log(1/eps));   % max x to get tails to emach
-      case 2        % Matern nu=1/2
+      case 2
+        nam = 'Matern nu=1/2';
         ker = Matern12_ker(dim,l);
         h = l/5;    % crappy convergence
         tol = 1e-5; L = l*log(1/tol);   % max x to get tails to err tol
@@ -25,6 +27,7 @@ for type=1:2   % -------------- kernel types --------------
     n = ceil(2*L/h);
     x = -L+(0:n-1)*h;
     if dim==1
+      disp([nam, '...']);
       khatap = h*sum(ker.k(abs(x)).*cos(2*pi*xid*x));
     elseif dim==2 
       [xx yy] = ndgrid(x,x);
