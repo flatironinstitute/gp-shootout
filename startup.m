9aad97702d728757b596260251e1d8674da7d774
ahbarnett <alex.h.barnett@gmail.com>
2022-04-08 10:27:58 -0400

    startup, readme, just random tweaks

--- startup.m
+++ startup.m
@@ -1,9 +1,5 @@
 % global matlab setup
 
-% terminal style
-format long g
-format compact
-
 % point to library locations here, based on machine name
 [status,host] = system('uname -n');
 if strcmp(host(1:4),'ross')     % alex's setup
@@ -16,3 +12,14 @@ end
 h = fileparts(mfilename('fullpath'));
 addpath(genpath(h))                        % gives access to all subdirs
 rmpath(genpath(fullfile(h,'.git')))
+
+% terminal style
+format long g
+format compact
+
+% restore pre-R2014b figure look:
+set(groot,'DefaultFigureGraphicsSmoothing','off')   % kill slow antialiasing
+set(groot,'DefaultAxesColorOrder',[0 0 1; 0 .5 0; 1 0 0; 0 .75 .75; ...
+                                   .75 0 .75; .75 .75 0; .25 .25 .25])
+set(groot,'DefaultFigureColormap',jet(256))
+set(groot,'defaultLegendAutoUpdate','off')
