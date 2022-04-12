451249d303b5052ad222860a376fdac63435543a
ahbarnett <alex.h.barnett@gmail.com>
2022-04-11 21:33:23 -0400

    updated doc for Matern_ker for Philip general-nu arg list

--- kernels/Matern_ker.m
+++ kernels/Matern_ker.m
@@ -1,10 +1,15 @@
 function ker = Matern_ker(dim, nu, l)
-% MATERN12_KER   Matern nu=1/2 kernel func and Fourier transform, general dim
+% MATERN_KER   Matern kernel func and Fourier transform, general nu and dim.
 %
-% ker = Matern12_ker(dim,l) returns ker.k a function handle taking an array of
-%  distances to kernel values, and ker.khat, the function handle to its
-%  Fourier transform (mapping wavevector magnitudes to values).
-%  Latter uses Phillip's FT convention. l is the distance scale, aka rho
+% ker = Matern_ker(dim,nu,l) returns the Matern kernel in general dimension dim,
+%  with various nu parameters, and general lengthscale l (a.k.a. rho).
+%  A struct is returned with at least the fields:
+%    k -    a function handle taking an array of distances to kernel values.
+%    khat - a function handle to its d-dim isotropic Fourier transform (mapping
+%           wavevector magnitudes to values).
+%    fam -  a descriptive string.
+%  nu can take one of only three values: 1/2, 3/2, or 5/2, currently.
+%  See the paper for the Fourier transform convention (the 2pi is "upstairs").
 %
 % To test see: TEST_KERS
 
@@ -26,9 +31,8 @@ end
 % following taken from philip nufft_gps2d:  
 scaling = (2*sqrt(pi))^dim * gamma(nu+dim/2) * (2*nu)^nu / (gamma(nu) * l^(2*nu));   % yuk; see https://en.wikipedia.org/wiki/Mat%C3%A9rn_covariance_function
 ker.khat = @(xid) scaling * (2*nu/l^2 + (4*pi^2) * xid.^2).^(-(nu + dim/2));
-% xid means |xi|
+% xid means |xi|, ie the "distance" from origin in Fourier space.
 
 % attributes to extract when using certain algorithms (e.g. ski)
 ker.l = l;
 ker.nu = nu;
-
