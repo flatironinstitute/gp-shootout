bfe745b56cb85fb8283f22b2a1551c0c76529a78
ahbarnett <alex.h.barnett@gmail.com>
2022-04-20 00:24:33 -0400

    expt_heaton sim now reasonable w/ EFGP

--- expt_heaton.m
+++ expt_heaton.m
@@ -2,20 +2,27 @@
 % The Heaton et al. 2019 paper doesn't state the best/used kernel params for
 % this data, but we pulled them out of the github repo.
 % Barnett, April 2022.
+%
+% sim decent sigma~0.6 now: (but tol is not indicative, due to ill-cond still):
+%  1 sec      tol = 1e-4. (302 its, 30k nodes),   MAE = 0.9
+%  6 sec      tol = 3e-5  (420 its, 113k nodes)  MAE = 0.68, ok
+%  25 sec for tol=1e-5, (611 its, 380k nodes)   MAE = 0.615,  good.
+%  150 sec for tol =3e-6, (814 iters, 1.4M nodes).  MAE = 0.607
+%  (we see y rms resid stil dropping O(1), from 0.42 to 0.35 in last step)
 
 clear; verb = 1;
 [x,meas,~,xtrg,truetrg] = get_Heatondata('sim');
+dim = 2;
 
 % kernel params (Heaton claim to have fitted) to give as in line 110 of:
 %https://github.com/finnlindgren/heatoncomparison/blob/master/Code/FormatData/FormatData.R
-mu = 44.49105;      % prior non-zero mean of GP (const in space, by why not try linear, since obvious SW-increasing trend?)
-sigma = 1;       % in degree C units; 0.05 too small given Wan's MODIS doc
-var = 16.40771;     % overall prior variance = covar kernel C(0)
-l = 1/0.75;   % kernel lengthscale in "degree" units; ignore wrong LON scale :(
-dim = 2; nu = 1/2;
-%ker = Matern_ker(dim,nu,l,var);
-ker = SE_ker(dim,l,var);
-opts.tol = 1e-7;    % alg param: has big effect on CPU time for nu=1/2
+mu = 44.49105;      % prior non-zero mean of GP (const in space, apparently)
+sigma = 0.6;        % in degree C units; 0.05 too small given Wan's MODIS doc
+                    % furthermore, sigma=0.05 is unstable (8e3 iters, beta big)
+var = 16.40771;     % overall prior variance = covar kernel C(0) (sig rel to it)
+l = 1/0.75;   % Matern kernel length in degrees; ignore wrong LON scale :(
+nu = 1/2; ker = Matern_ker(dim,nu,l,var); opts.tol = 3e-5;  % tol<1e-5 hurts!
+%l = 0.1; ker = SE_ker(dim,l,var); opts.tol = 1e-7;  % try easier kernel
 
 N = numel(meas); Ntrg = numel(truetrg);
 fprintf('\ntry mu-shifted EFGP on Heaton: N=%d (Ntrg=%d), sigma=%.3g, tol=%.3g...\n',N,Ntrg,sigma,opts.tol)
@@ -24,9 +31,11 @@ tic;   % now do GP regression with non-zero mu handled by subtraction...
 y.mean = y.mean + mu; ytrg.mean = ytrg.mean + mu;  % add mu back in
 fprintf('done in %.3g sec: \t%d iters, %d xi-nodes, rms(beta)=%.3g\n',toc,info.iter,numel(info.xis)^dim,rms(info.beta))
 fprintf('CPU time breakdown (s):'); fprintf('\t%.3g',info.cpu_time); fprintf('\n');
+% extract a good choice for sigma, around 0.4-0.6... *not* 0.05!
+fprintf('rms resid @ x train pts:\t%.3g (degrees C, suggests sigma)\n', rms(y.mean-meas))
 
 MAE = mean(abs(ytrg.mean-truetrg));
-fprintf('MAE @ test targs: %.3g (degrees Celsius or K; aiming for 0.6)\n', MAE)
+fprintf('MAE @ test targs:       \t%.3g (degrees C; aiming for 0.6)\n', MAE)
 
 if verb, figure;
   xa = [x, xtrg];        % gather all nodes
@@ -38,5 +47,3 @@ if verb, figure;
   caxis(c); colorbar
   title('pred mean targ data');
 end
-
-% *** to do, check tol effect on y.mean, to check convergence
