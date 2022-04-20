e94920526c56e36fa5ec69ebea6b41ca5e3bdb8b
ahbarnett <alex.h.barnett@gmail.com>
2022-04-20 00:35:39 -0400

    good Heaton '19 EFGP fitting params sim and sat; time v sens to tol; sigma = 0.4 or 0.9

--- expt_heaton.m
+++ expt_heaton.m
@@ -3,25 +3,30 @@
 % this data, but we pulled them out of the github repo.
 % Barnett, April 2022.
 %
-% sim decent sigma~0.6 now: (but tol is not indicative, due to ill-cond still):
+% 'sim' decent sigma~0.6 now (but tol is not indicative, due to ill-cond still):
 %  1 sec      tol = 1e-4. (302 its, 30k nodes),   MAE = 0.9
 %  6 sec      tol = 3e-5  (420 its, 113k nodes)  MAE = 0.68, ok
 %  25 sec for tol=1e-5, (611 its, 380k nodes)   MAE = 0.615,  good.
-%  150 sec for tol =3e-6, (814 iters, 1.4M nodes).  MAE = 0.607
+%  150 sec for tol =3e-6, (814 iters, 1.4M nodes).  MAE = 0.607 (best in paper).
 %  (we see y rms resid stil dropping O(1), from 0.42 to 0.35 in last step)
 
+% for 'sat', sigma=0.9 tol=1e-5 gets 0.88 resid, 1.36 MAE, in 22 sec (500 its)
+
 clear; verb = 1;
-[x,meas,~,xtrg,truetrg] = get_Heatondata('sim');
+type = 'sat';
+[x,meas,~,xtrg,truetrg] = get_Heatondata(type);
 dim = 2;
 
-% kernel params (Heaton claim to have fitted) to give as in line 110 of:
+% kernel params (Heaton et al claim to have fitted) to give as in line 110 of:
 %https://github.com/finnlindgren/heatoncomparison/blob/master/Code/FormatData/FormatData.R
 mu = 44.49105;      % prior non-zero mean of GP (const in space, apparently)
-sigma = 0.6;        % in degree C units; 0.05 too small given Wan's MODIS doc
+if strcmp(type,'sim'), sigma = 0.4; else, sigma = 0.9; end  % estim by resid
+%sigma = 1.0;        % degree C units; 0.05 way too small, given Wan's MODIS doc
                     % furthermore, sigma=0.05 is unstable (8e3 iters, beta big)
 var = 16.40771;     % overall prior variance = covar kernel C(0) (sig rel to it)
 l = 1/0.75;   % Matern kernel length in degrees; ignore wrong LON scale :(
-nu = 1/2; ker = Matern_ker(dim,nu,l,var); opts.tol = 3e-5;  % tol<1e-5 hurts!
+
+nu = 1/2; ker = Matern_ker(dim,nu,l,var); opts.tol = 1e-5;  % tol<1e-5 hurts!
 %l = 0.1; ker = SE_ker(dim,l,var); opts.tol = 1e-7;  % try easier kernel
 
 N = numel(meas); Ntrg = numel(truetrg);
@@ -35,7 +40,7 @@ fprintf('CPU time breakdown (s):'); fprintf('\t%.3g',info.cpu_time); fprintf('\n
 fprintf('rms resid @ x train pts:\t%.3g (degrees C, suggests sigma)\n', rms(y.mean-meas))
 
 MAE = mean(abs(ytrg.mean-truetrg));
-fprintf('MAE @ test targs:       \t%.3g (degrees C; aiming for 0.6)\n', MAE)
+fprintf('MAE @ test targs:       \t%.3g (degrees C; sim aims for 0.6)\n', MAE)
 
 if verb, figure;
   xa = [x, xtrg];        % gather all nodes
