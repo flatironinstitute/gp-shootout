9fb1e4e5c6ea578c9214306c7d12a248dd7fe7fd
ahbarnett <alex.h.barnett@gmail.com>
2022-04-29 18:01:27 -0400

    RLCM wrapped; submodule changed to ahbarnett fork

--- algs/RLCM/buildit.sh
+++ algs/RLCM/buildit.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-# compiles Jie Chen's RLCM package. We need the "apps"
+# compiles Jie Chen's RLCM package. We need the "apps", and we added
+# wrappers to the SE kernel there which use IO via binary files...
 cp -f configure_alexhack.sh RLCM
 (cd RLCM; ./configure_alexhack.sh && make && make install && make apps)
-
-# compile our wrapper around it.
-# *** need cpp wrapper which reads binary (train, test x), writes to binary
-# (pred y, pred ytrg).  Since his .ex app files don't write out the answer!
-
-
-
-
